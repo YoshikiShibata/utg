b9e394316b4b6be4bf0a278888101078695b26f0
YoshikiShibata <yshibata@ca2.so-net.ne.jp>
2013-04-13 11:38:03 +0900

    added asterisk escape

--- grep/grep.go
+++ grep/grep.go
@@ -6,6 +6,7 @@ import "fmt"
 import "os"
 import "bufio"
 import "regexp"
+import "io/ioutil"
 
 // This is a simple "grep" command implementation
 // Each specified file will be examined by a goroutine assigned for the file.
@@ -73,7 +74,49 @@ func expandFiles(files []string) []string {
 func expandFile(file string) []string {
     result := make([]string, 0, 1)
 
-    return append(result, file);
+	if !containsAsterisks(file) {
+    	return append(result, file);
+	}
+
+	pattern := toRegexPattern(file)
+	compiledPattern, err := regexp.Compile(pattern)
+    if err != nil {
+        fmt.Printf("Illegal pattern (%s) : %s\n", pattern, err.Error())
+        os.Exit(1)
+    }
+
+
+	fileInfos, _ := ioutil.ReadDir(".")
+	for _, fileInfo := range fileInfos {
+		fileName := fileInfo.Name()
+		if compiledPattern.MatchString(fileName) {
+			result = append(result, fileName)
+		}
+	}
+
+	return result
+}
+
+func containsAsterisks(fileName string) bool {
+	for _, ch := range fileName {
+		if ch == '*' {
+			return true
+		}
+	}
+	return false
+}
+
+func toRegexPattern(fileName string) string {
+	runes := make([]rune, 0, len(fileName))
+	for _, ch := range fileName {
+		if ch == '*' {
+			runes = append(runes,'.')
+			runes = append(runes,'*')
+		} else {
+			runes = append(runes, ch)
+		}
+	}
+	return string(runes)
 }
 
 func showResults(results []chan grepResult) {
@@ -123,7 +166,6 @@ func grepPatternFromOneFile(pattern *regexp.Regexp,
             result.lineNumber = lineNumber
             result.line = fullLine
             resultChan <- result
-			fmt.Printf("written to the channel\n")
         }
     }
 
